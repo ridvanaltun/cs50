f246a60e584bbfd4078cab56e57af7698daafa49
Rıdvam Altun <ridvanaltun@outlook.com>
2019-06-04 23:01:48 +0300

    solved

---
+++ pset1/mario.c
@@ -0,0 +1,73 @@
+/**
+ * Solution of Mario Assignment for Harvard CS50 Class
+ *
+ * Name  : Ridvan Altun
+ * Date  : 06/04/2019
+ * Email : ridvanaltun@outlook.com
+ */
+
+#include <cs50.h>
+#include <stdio.h>
+
+// Prototypes
+int get_height(int min, int max);
+void print_hash_bricks(int height);
+
+int main(void)
+{
+    int height = get_height(1, 8);
+    print_hash_bricks(height);
+}
+
+// Get value from the user until get an appropriate value.
+int get_height(int min, int max)
+{
+    int num;
+	
+    do
+    {
+       num = get_int("Height : "); 
+    }
+	while (num < min || num > max);
+	
+    return num;
+}
+
+// Print hash '#' bricks on terminal.
+void print_hash_bricks(int height)
+{
+    for (int i = 0; i < height; i++)
+    {
+        // Print left bricks.
+        for (int j = 0; j < height; j++)
+        {
+            if(height - 1 - i > j)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("#");
+            }
+        }
+        
+        // Print middle space.
+        printf("  ");
+        
+        // Print right bricks.
+        for (int k = 0; k < height; k++)
+        {
+            if(i < k)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("#");
+            }
+        }
+		
+		// Jump next line.
+        printf("\n");
+    }
+}
